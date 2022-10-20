878a20e57af8b99846e8c2f8d95b980e8173f494
Victor Davydenko <acc.davydenko@gmail.com>
2022-10-20 20:47:25 +0300

    errors fixed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,12 +1,12 @@
-import Sequelize from 'sequelize'
-import sequelize from '../utils/database.js'
-import token from './tokenModel.js'
+import Sequelize from 'sequelize';
+import sequelize from '../utils/database.js';
+import token from './tokenModel.js';
 
 const user = sequelize.define('User', {
   email: {
     type: Sequelize.STRING,
     allowNull: false,
-    unique: true
+    unique: true,
   },
   name: {
     type: Sequelize.STRING,
@@ -14,16 +14,16 @@ const user = sequelize.define('User', {
     validate: {
       len: {
         args: [2],
-        msg: 'Too short'
-      }
-    }
+        msg: 'Too short',
+      },
+    },
   },
   password: {
     type: Sequelize.STRING,
-    allowNull: false
-  }
-})
+    allowNull: false,
+  },
+});
 
-user.hasOne(token, { onDelete: "cascade" })
+user.hasOne(token, { onDelete: 'cascade' });
 
-export default user
+export default user;
