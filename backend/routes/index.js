0f5c7bf4c5ceb6a27c2438544cce31a443cb278c
Victor Davydenko <acc.davydenko@gmail.com>
2022-11-01 14:16:12 +0200

    routes for working with documents added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import userController from '../controllers/userController.js';
+import documentController from '../controllers/documentController.js';
 import authMiddleware from '../middleware/auth.js';
 
 const router = new Router();
@@ -8,6 +9,15 @@ router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/refresh', userController.refresh);
+router.post('/add_contract', authMiddleware, documentController.addContract);
+router.put('/update_contract/:id', authMiddleware, documentController.updateContract);
+router.delete('/remove_contract/:id', authMiddleware, documentController.removeContract);
+router.get('/get_contract/:id', authMiddleware, documentController.getContract);
+router.post('/add_completedWorkAct', authMiddleware, documentController.addCompletedWorkAct);
+router.put('/update_completedWorkAct/:id', authMiddleware, documentController.updateCompletedWorkAct);
+router.delete('/remove_completedWorkAct/:id', authMiddleware, documentController.removeCompletedWorkAct);
+router.get('/get_completedWorkAct/:id', authMiddleware, documentController.getCompletedWorkAct);
+router.get('/all_documents', authMiddleware, documentController.getAllDocuments);
 router.get('/user', authMiddleware, userController.user);
 
 export default router;
